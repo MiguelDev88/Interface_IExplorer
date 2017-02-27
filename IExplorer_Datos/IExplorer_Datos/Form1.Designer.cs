fa9ad65bef5eaf82d474c4c5a5237dd4ed61f4f7
MiguelDev88 <mpereirafern@gmail.com>
2017-02-27 18:58:32 +0100

    Finished main building

--- IExplorer_Datos/IExplorer_Datos/Form1.Designer.cs
+++ IExplorer_Datos/IExplorer_Datos/Form1.Designer.cs
@@ -31,14 +31,22 @@
             this.pbPhone = new System.Windows.Forms.PictureBox();
             this.lbPhoneName = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pbStorage = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbPhone)).BeginInit();
             this.SuspendLayout();
             // 
             // pbPhone
             // 
-            this.pbPhone.Location = new System.Drawing.Point(124, 37);
+            this.pbPhone.Location = new System.Drawing.Point(12, 12);
             this.pbPhone.Name = "pbPhone";
-            this.pbPhone.Size = new System.Drawing.Size(100, 180);
+            this.pbPhone.Size = new System.Drawing.Size(77, 142);
             this.pbPhone.TabIndex = 0;
             this.pbPhone.TabStop = false;
             // 
@@ -46,7 +54,7 @@
             // 
             this.lbPhoneName.AutoSize = true;
             this.lbPhoneName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbPhoneName.Location = new System.Drawing.Point(277, 40);
+            this.lbPhoneName.Location = new System.Drawing.Point(160, 12);
             this.lbPhoneName.Name = "lbPhoneName";
             this.lbPhoneName.Size = new System.Drawing.Size(155, 24);
             this.lbPhoneName.TabIndex = 1;
@@ -56,22 +64,106 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(281, 92);
+            this.label1.Location = new System.Drawing.Point(160, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Capacity:";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Software Version";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(160, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Firmware Version:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Serial Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(160, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Phone Number:";
+            // 
+            // pbStorage
+            // 
+            this.pbStorage.Location = new System.Drawing.Point(31, 175);
+            this.pbStorage.Name = "pbStorage";
+            this.pbStorage.Size = new System.Drawing.Size(324, 23);
+            this.pbStorage.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(231, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "60.4 GB";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "5.5 GB Used";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(281, 180);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "56.8 GB Free";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1003, 291);
+            this.ClientSize = new System.Drawing.Size(383, 291);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pbStorage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbPhoneName);
             this.Controls.Add(this.pbPhone);
             this.Name = "Form1";
             this.Text = "IExplorer_Data";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbPhone)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -83,6 +175,14 @@
         private System.Windows.Forms.PictureBox pbPhone;
         private System.Windows.Forms.Label lbPhoneName;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar pbStorage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
